ce6d3d1a10aa04c90d7fc27416eb84bdfd16a672
Scott <scott.a.shannon@outlook.com>
2023-08-24 10:54:27 -0700

    add feeback form, stats, and list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+import { v4 as uuid4} from 'uuid';
 import Header from "./components/Header";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')){
-        setFeedback(feedback.filter((item) => item.id !== id))
-    }
+
+
+  const addFeedback =  (newFeedback) => {
+      newFeedback.id = uuid4()
+      console.log(newFeedback)
   }
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/> 
+
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
